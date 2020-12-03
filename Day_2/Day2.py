80a1a1b4dc38b6a7bfd11a717b7b87157c27dc79
Hojjat Seyed <hojjatseyed@gmail.com>
2020-12-03 11:05:08 +0100

    Day 3 solutions added

--- Day_2/Day2.py
+++ Day_2/Day2.py
@@ -23,7 +23,7 @@ def part_two():
         s = s.split()
         policy_range = s[0].split('-')
         policy_char = s[1][0]
-        if s[2].count(policy_char, int(policy_range[0])-1, int(policy_range[0])) + s[2].count(policy_char, int(policy_range[1])-1, int(policy_range[1])) == 1:
+        if (s[2][int(policy_range[0])-1] == policy_char) ^ (s[2][int(policy_range[1])-1] == policy_char):
             valids += 1
     return valids
 
---
+++ Day_3/Day3.py
@@ -0,0 +1,21 @@
+# Input for part 1 and 2
+with open('input.txt', 'r') as puzzle_input:
+    input_list = [line.strip() for line in puzzle_input.readlines()]
+
+
+def count_trees(right, down):
+    trees = 0
+    spot = 0
+    for line in input_list[::down]:
+        row_width = len(line)
+        if (spot >= row_width):
+            spot = spot % row_width
+        if line[spot] == '#':
+            trees += 1
+        spot += right
+    return trees
+
+
+print('Part 1: ', count_trees(3, 1))
+print('Part 2: ', count_trees(1, 1)*count_trees(3, 1) *
+      count_trees(5, 1)*count_trees(7, 1)*count_trees(1, 2))
