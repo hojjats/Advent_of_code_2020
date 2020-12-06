56b516b89ee098c2633d79ec42ef79123eed84ff
Hojjat Seyed <hojjatseyed@gmail.com>
2020-12-06 11:26:32 +0100

    Day 6 solutions added

---
+++ Day_6/Day6.py
@@ -0,0 +1,19 @@
+# Day 6
+def part_one():
+    count = 0
+    for line in open('input.txt').read().split("\n\n"):
+        count += len(set(line.replace('\n', '')))
+    return count
+
+
+def part_two():
+    count = 0
+    for line in open('input.txt').read().split('\n\n'):
+        for i in set(line.replace('\n', '')):
+            if line.count(i) == len(line.split('\n')):
+                count += 1
+    return count
+
+
+print('Part 1:', part_one())
+print('Part 2:', part_two())
